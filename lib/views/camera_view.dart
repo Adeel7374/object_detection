9cc4f703fa0c5506018497c8c48722ecd387d1aa
Adeel7374 <mianadee25@gmail.com>
2023-07-20 14:54:09 +0500

    ok

--- lib/views/camera_view.dart
+++ lib/views/camera_view.dart
@@ -4,7 +4,7 @@ import 'package:get/get.dart';
 import 'package:object_detection/controllers/scan_controller.dart';
 
 class CameraView extends StatelessWidget {
-  const CameraView({super.key});
+  const CameraView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -13,7 +13,32 @@ class CameraView extends StatelessWidget {
         init: ScanController(),
         builder: (controller) {
           return controller.isCameraInitialized.value
-              ? CameraPreview(controller.cameraController)
+              ? Stack(
+                  children: [
+                    CameraPreview(controller.cameraController),
+                    Positioned(
+                      top: (controller.y) * 700,
+                      right: (controller.x) * 500,
+                      child: Container(
+                        width: controller.w * 100 * context.width / 100,
+                        height: controller.h * 100 * context.height / 100,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(8),
+                          border: Border.all(color: Colors.green, width: 4.0),
+                        ),
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Container(
+                              color: Colors.white,
+                              child: Text(controller.label),
+                            )
+                          ],
+                        ),
+                      ),
+                    )
+                  ],
+                )
               : const Center(child: Text("Loading Preview..."));
         },
       ),
